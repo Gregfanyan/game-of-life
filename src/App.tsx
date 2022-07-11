5b6348aa5d6e467ed7b4014b167a724fdd1432f7
Gregfanyan <grigorfanyan@gmail.com>
2022-07-11 11:35:12 +0200

    ✨ added basic game structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+const numOfRows = 25;
+const numOfCols = 35;
+
+const randomGrid = () => {
+  const grid = [];
+  for (let i = 0; i < numOfRows; i++) {
+    const row = [];
+    for (let j = 0; j < numOfCols; j++) {
+      row.push(Math.floor(Math.random() * 2));
+    }
+    grid.push(row);
+  }
+  return grid;
+};
+
 function App() {
-  return <div className="App">game of life</div>;
+  const [grid, setGrid] = useState<number[][]>(randomGrid());
+  console.log("grid", grid);
+  let newGrid = [...grid];
+  console.log("newGrid", newGrid);
+  return (
+    <>
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          padding: "20px",
+        }}
+      >
+        Game of Life
+      </h1>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${numOfCols},  20px) `,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {grid &&
+          grid.map((rows, rowIndex) =>
+            rows.map((col, colIndex) => {
+              return (
+                <div
+                  key={`${rowIndex}-${colIndex}`}
+                  style={{
+                    width: 20,
+                    height: 20,
+                    backgroundColor: grid[rowIndex][colIndex]
+                      ? "#13d626"
+                      : undefined,
+                    border: "1px solid #595959",
+                  }}
+                />
+              );
+            })
+          )}
+      </div>
+    </>
+  );
 }
 
 export default App;
